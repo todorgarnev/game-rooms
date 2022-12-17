6b8847dd169fdab386dbfa084d3af5581cb12640
Todor Garnev <tgarnev@abv.bg>
2022-12-17 19:20:27 +0200

    Added progressive enhancement on login form.

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,13 +1,42 @@
 <script lang="ts">
+	import { enhance, type SubmitFunction } from "$app/forms";
+	import { supabaseClient } from "$lib/supabase";
+	import { AuthApiError } from "@supabase/supabase-js";
+	import { redirect } from "@sveltejs/kit";
+
+	export let error: string = "";
+
+	const submitLogin: SubmitFunction = async ({ data, cancel }) => {
+		const body = Object.fromEntries(data);
+		const { error: err } = await supabaseClient.auth.signInWithPassword({
+			email: body.email as string,
+			password: body.password as string
+		});
+
+		if (err) {
+			error =
+				err instanceof AuthApiError && err.status === 400
+					? "Wrong credentials"
+					: "Server error. Try again later.";
+		}
+
+		redirect(303, "/");
+	};
 </script>
 
 <main>
 	<h1>Login</h1>
-	<form action="?/login" method="POST" class="auth-form">
+	<form action="?/login" method="POST" class="auth-form" use:enhance={submitLogin}>
 		<label for="email"> Email </label>
 		<input type="text" name="email" />
+
 		<label for="password"> Password </label>
 		<input type="password" name="password" />
+
+		{#if error}
+			<p>{error}</p>
+		{/if}
+
 		<button type="submit" class="btn btn-primary">Login</button>
 	</form>
 </main>
