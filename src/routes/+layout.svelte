74cdc9a3dd2b0d4ede0b153985b2d56eac299348
Todor Garnev <tgarnev@abv.bg>
2022-12-17 14:58:03 +0200

    Added supabase client.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,20 @@
-<script>
-	import ("../app.css")
+<script lang="ts">
+	import { onMount } from "svelte";
+	import { invalidateAll } from "$app/navigation";
+	import { supabaseClient } from "src/lib/supabase";
+	import("../app.css");
+
+	onMount(() => {
+		const {
+			data: { subscription }
+		} = supabaseClient.auth.onAuthStateChange(() => {
+			invalidateAll();
+		});
+
+		return () => {
+			subscription.unsubscribe();
+		};
+	});
 </script>
 
 <slot />
