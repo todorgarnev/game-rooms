4c0b2127b26fcd696226dd426902f2c519e1b188
Todor Garnev <tgarnev@abv.bg>
2022-12-17 20:38:21 +0200

    Added login/register routes check.

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,6 +1,12 @@
 import { AuthApiError } from "@supabase/supabase-js";
 import { fail, redirect } from "@sveltejs/kit";
-import type { Actions } from "./$types";
+import type { Actions, PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async ({ locals }) => {
+	if (locals.session) {
+		throw redirect(303, "/");
+	}
+};
 
 export const actions: Actions = {
 	register: async ({ request, locals }) => {
