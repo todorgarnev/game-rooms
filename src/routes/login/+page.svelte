1eee73d236de0be1a6934ad4874e69fd36d9e2b3
Todor Garnev <tgarnev@abv.bg>
2022-12-17 15:46:34 +0200

    Added login logic.

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,11 +3,11 @@
 
 <main>
 	<h1>Login</h1>
-	<form action="" class="auth-form">
-		<label for=""> Email </label>
+	<form action="?/login" method="POST" class="auth-form">
+		<label for="email"> Email </label>
 		<input type="text" name="email" />
-		<label for=""> Password </label>
+		<label for="password"> Password </label>
 		<input type="password" name="password" />
-		<button class="btn btn-primary">Login</button>
+		<button type="submit" class="btn btn-primary">Login</button>
 	</form>
 </main>
