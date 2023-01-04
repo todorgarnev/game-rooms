ebbf8c5846d158318bd21f3f8af639cde9dc0f8b
Todor Garnev <tgarnev@abv.bg>
2023-01-04 18:45:21 +0200

    Added welcome text in home page; Added game rules to home page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
+	import { slide } from "svelte/transition";
 	import type { PageData } from "./$types";
 
 	export let data: PageData;
+	export let animate: boolean = false;
+
+	const toggleAnimation = () => {
+		animate = !animate;
+	};
 </script>
 
 <svelte:head>
@@ -16,19 +22,78 @@
 			You should set your username first. <br /> You can do it in <b>Profile</b> page or click
 			<a href="/profile">here</a>
 		</div>
+	{:else}
+		<div class="rules">
+			Here you can create a <a href="/rooms">room</a> or join already existing one. <br />
+			There you can start a game against another user. <br />
+			The game is Rock, Paper, Scissors, Lizard, Spock. <br />
+
+			<details on:click={toggleAnimation} on:keydown>
+				<summary>Rules</summary>
+
+				{#if animate}
+					<div transition:slide>
+						The game is an expansion on the game Rock, Paper, Scissors. <br />
+						Each player picks a variable and reveals it at the same time. <br />
+						The winner is the one who defeats the others. <br />
+						In a tie, the process is repeated until a winner is found.
+
+						<ul>
+							<li>Scissors cuts Paper</li>
+							<li>Paper covers Rock</li>
+							<li>Rock crushes Lizard</li>
+							<li>Lizard poisons Spock</li>
+							<li>Spock smashes Scissors</li>
+							<li>Scissors decapitates Lizard</li>
+							<li>Lizard eats Paper</li>
+							<li>Paper disproves Spock</li>
+							<li>Spock vaporizes Rock</li>
+							<li>Rock crushes Scissors</li>
+						</ul>
+					</div>
+				{/if}
+			</details>
+		</div>
 	{/if}
 {:else}
-	<p>Let's learn how to register and login users!</p>
+	<p>
+		You can register from <a href="/register">here</a>. <br />
+		If you already have an account you can login <a href="/login">here</a>.
+	</p>
 {/if}
 
 <style lang="postcss">
+	.rules,
+	p {
+		font-size: 2rem;
+	}
+
+	a,
+	b {
+		color: var(--primary-100);
+	}
+
+	details {
+		margin-top: 5rem;
+
+		&:hover {
+			cursor: pointer;
+		}
+
+		& ul {
+			list-style: none;
+			font-size: 1.8rem;
+			font-style: italic;
+		}
+	}
+
 	.set-username {
 		margin-top: 5rem;
 		font-size: 2rem;
+	}
 
-		& a,
-		& b {
-			color: var(--primary-100)
-		}
+	.rules,
+	ul {
+		margin-top: 5rem;
 	}
 </style>
